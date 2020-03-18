2b544628d9db88b6618d2831379f10be3e5fb5da
ZHAO Shuai <roy.zhao@essilorchina.com>
2020-03-18 15:04:04 +0800

    Add project files.

---
+++ TopicSender/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopicSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Send();
+        }
+        static void Send()
+        {
+            TopicClient topicClient;
+            String Message = "Hello Subscription!";
+            String connString = "Endpoint=sb://serivcebus1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fhZTCK4lF/GwtLL/j9q7X0TQkhG+6ejNvhVzFl+IaIY=";
+            NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(connString);
+            if (!namespaceManager.TopicExists("zstopic"))
+            {
+                TopicDescription topicDescription = new TopicDescription("zstopic");
+                topicDescription.AutoDeleteOnIdle = TimeSpan.FromMinutes(60);
+                namespaceManager.CreateTopic(topicDescription);
+            }
+            //创建TopicClient和SubscriptionClient
+            topicClient = TopicClient.CreateFromConnectionString(connString, "zstopic");
+            BrokeredMessage message = new BrokeredMessage(Message);
+            topicClient.Send(message);
+            topicClient.Close();
+            Console.WriteLine("send ok");
+        }
+    }
+}
